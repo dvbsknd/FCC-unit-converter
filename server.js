da7d84d4786e63edb14826a92584ea449833b4c3
Dave Baskind <dave@picketstudio.com>
2020-09-29 07:47:13 +1000

    Tidy linting errors from boilerplate

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 
 var express     = require('express');
 var bodyParser  = require('body-parser');
-var expect      = require('chai').expect;
 var cors        = require('cors');
 
 var apiRoutes         = require('./routes/api.js');
@@ -31,7 +30,7 @@ fccTestingRoutes(app);
 apiRoutes(app);  
     
 //404 Not Found Middleware
-app.use(function(req, res, next) {
+app.use(function(req, res) {
   res.status(404)
     .type('text')
     .send('Not Found');
