e40bcc36e01ca02cdea694d6d1410737e0ddec5a
Dave Baskind <dave@picketstudio.com>
2020-09-29 09:26:52 +1000

    Handle simple conversions

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,50 +1,44 @@
-/*
-*
-*
-*       Complete the handler logic below
-*       
-*       
-*/
+function ConvertHandler () {
+  
+  const re = /(\d+\.?\d*\/?\d*)(\w+)/;
+  const units = {
+    'gal': ['L', 'gallons', 3.78541],
+    'lbs': ['kg', 'pounds', 0.453592],
+    'mi': ['km', 'miles', 1.60934],
+    'L': ['gal', 'litres', 1/1.60934],
+    'kg': ['lbs', 'kilograms', 1/0.453592],
+    'km': ['mi', 'kilometres', 1/1.60934],
+  }
 
-function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    var result;
-    
-    return result;
+  this.getNum = (input) =>  {
+    // Should return only the number component of the input
+    return Number(input.match(re)[1]);
   };
   
-  this.getUnit = function(input) {
-    var result;
-    
-    return result;
+  this.getUnit = (input) =>  {
+    // Should return only the supplied unit
+    return input.match(re)[2];
   };
   
-  this.getReturnUnit = function(initUnit) {
-    var result;
-    
-    return result;
+  this.getReturnUnit = (initUnit) =>  {
+    // Should return the target unit based on the input one
+    return units[initUnit][0];
   };
 
-  this.spellOutUnit = function(unit) {
-    var result;
-    
-    return result;
+  this.spellOutUnit = (unit) =>  {
+    // Return the full spelling of a given unit
+    return units[unit][1];
   };
   
-  this.convert = function(initNum, initUnit) {
-    const galToL = 3.78541;
-    const lbsToKg = 0.453592;
-    const miToKm = 1.60934;
-    var result;
-    
-    return result;
+  this.convert = (initNum, initUnit) =>  {
+    // Convert supplied number and unit to its equivalent
+    return initNum * units[initUnit][2];
   };
   
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    var result;
-    
-    return result;
+  this.getString = (initNum, initUnit, returnNum, returnUnit) =>  {
+    const initUnitWord = this.spellOutUnit(initUnit);
+    const returnUnitWord = this.spellOutUnit(returnUnit);
+    return `${initNum} ${initUnitWord} converts to ${returnNum} ${returnUnitWord}`;
   };
   
 }
--- routes/api.js
+++ routes/api.js
@@ -1,30 +1,20 @@
-/*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
-
 'use strict';
 
-var expect = require('chai').expect;
-var ConvertHandler = require('../controllers/convertHandler.js');
+const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
-  
-  var convertHandler = new ConvertHandler();
+
+  const convertHandler = new ConvertHandler();
 
   app.route('/api/convert')
-    .get(function (req, res){
-      var input = req.query.input;
-      var initNum = convertHandler.getNum(input);
-      var initUnit = convertHandler.getUnit(input);
-      var returnNum = convertHandler.convert(initNum, initUnit);
-      var returnUnit = convertHandler.getReturnUnit(initUnit);
-      var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
-      
-      //res.json
+    .get((req, res) => {
+      const input = req.query.input;
+      const initNum = convertHandler.getNum(input);
+      const initUnit = convertHandler.getUnit(input);
+      const returnUnit = convertHandler.getReturnUnit(initUnit);
+      const returnNum = convertHandler.convert(initNum, initUnit);
+      const toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+      res.json({ initNum, initUnit, returnNum, returnUnit, string: toString });
     });
-    
+
 };
